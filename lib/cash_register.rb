75fe208e066eb7b1c8886921feb3e0939b127e4e
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-26 00:05:24 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -14,7 +14,7 @@ class CashRegister
   def apply_discount
     if @discount != 0
       @total *= (1 - discount * 0.01)
-      return "After the discount, the total comes to #{@total}"
+      return "After the discount, the total comes to $#{@total.to_i}"
     else
       return "There is no discount to apply."
     end
